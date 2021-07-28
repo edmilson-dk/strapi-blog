7ef5461145df9cd9743fca87571bf907dd597b1e
edmilson-dk <droidvidaboa@gmail.com>
2021-07-28 10:29:14 -0300

    feat: added get initial props

--- front-end/src/pages/_app.js
+++ front-end/src/pages/_app.js
@@ -1,7 +1,53 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import App from "next/app";
+import Head from "next/head";
+import Script from "next/script";
 
-export default MyApp
+import { createContext } from "react";
+import { getStrapiMedia } from "../lib/media";
+import { fetchStrapiApi } from "../lib/strapi-api";
+
+// Store Strapi Global object in context
+export const GlobalContext = createContext({});
+
+const MyApp = ({ Component, pageProps }) => {
+  const { global } = pageProps;
+
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href={getStrapiMedia(global.favicon)} />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Staatliches"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css"
+        />
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js" />
+        <Script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js" />
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js" />
+      </Head>
+      <GlobalContext.Provider value={global}>
+        <Component {...pageProps} />
+      </GlobalContext.Provider>
+    </>
+  );
+};
+
+// getInitialProps disables automatic static optimization for pages that don't
+// have getStaticProps. So article, category and home pages still get SSG.
+// Hopefully we can replace this with getStaticProps once this issue is fixed:
+// https://github.com/vercel/next.js/discussions/10949
+MyApp.getInitialProps = async (ctx) => {
+  // Calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(ctx);
+  // Fetch global site settings from Strapi
+  const global = await fetchStrapiApi("/global");
+  // Pass the data to our page via props
+  return { ...appProps, pageProps: { global } };
+};
+
+export default MyApp;
